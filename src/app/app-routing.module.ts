7148341bbddfd3c3d851915535293bd694744902
Ethan Harvey <ethan.harvey@tufts.edu>
2022-06-30 12:18:34 -0400

    useHash: true

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,9 @@ const routes: Routes = [
   { path: 'admissions', component: AdmissionsComponent },
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
