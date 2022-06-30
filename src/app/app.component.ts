aa992365faaef2ef0178afb8d8e9686401828be9
Ethan Harvey <ethan.harvey@tufts.edu>
2022-06-30 10:41:43 -0400

    Rebuilt myApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   title = 'myApp';
 
   isHomePage(): boolean {
-    return window.location.href === "http://localhost:4200/" || window.location.href === "http://ethanharvey98.github.io";
+    return (window.location.href === "http://localhost:4200/" ||
+    window.location.href === "https://ethanharvey98.github.io");
   }
 }
