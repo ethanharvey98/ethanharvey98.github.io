ba58ff0f2d982ec724489c84661ffff47e6407a3
Ethan Harvey <ethan.harvey@tufts.edu>
2022-07-11 08:49:06 -0400

    A couple updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PeopleComponent } from './people/people.component';
-import { HciAtTuftsComponent } from './hci-at-tufts/hci-at-tufts.component';
 import { CodeAndDatasetsComponent } from './code-and-datasets/code-and-datasets.component';
-import { AdmissionsComponent } from './admissions/admissions.component';
 import { PublicationsComponent } from './publications/publications.component';
 import { ProjectsComponent } from './projects/projects.component';
 
@@ -14,9 +12,7 @@ import { ProjectsComponent } from './projects/projects.component';
   declarations: [
     AppComponent,
     PeopleComponent,
-    HciAtTuftsComponent,
     CodeAndDatasetsComponent,
-    AdmissionsComponent,
     PublicationsComponent,
     ProjectsComponent
   ],
