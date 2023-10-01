d9c4c63255f7ce2ef783d1a745112ee0046cac3c
Ethan Harvey <Ethan.Harvey@tufts.edu>
2023-09-30 20:33:56 -0400

    Added Last Updated:

---
+++ app.js
@@ -0,0 +1,17 @@
+const updateLastUpdated = async () => {
+    try {
+        const response = await fetch('https://api.github.com/repos/ethanharvey98/ethanharvey98.github.io/events');
+        const data = await response.json();
+        const pushEvent = data.find(event => event.type === 'PushEvent');
+        if (pushEvent) {
+            const lastUpdated = new Date(pushEvent.created_at);
+            const options = { year: 'numeric', month: 'long', day: 'numeric' };
+            const formattedDate = lastUpdated.toLocaleDateString(undefined, options);
+            document.getElementById('last-updated').textContent = `Last Updated: ${formattedDate}`;
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+updateLastUpdated();
